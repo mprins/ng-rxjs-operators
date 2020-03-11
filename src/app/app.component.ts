ca5e97b24853ee4f78c7797e1132e5e97f6590e1
PeterKassenaar <info@kassenaar.com>
2020-03-11 13:28:14 +0100

    update to @angular/cli V9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'main',
   templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class AppComponent {
 }
