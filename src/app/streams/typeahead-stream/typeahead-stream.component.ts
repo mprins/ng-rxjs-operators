984da6a68ec19f4adc05c804cef76b25b3e1ed3e
Peter Kassenaar <info@kassenaar.com>
2020-03-12 19:10:58 +0100

    Updated with typeahead example

--- src/app/streams/typeahead-stream/typeahead-stream.component.ts
+++ src/app/streams/typeahead-stream/typeahead-stream.component.ts
@@ -1,23 +1,19 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {debounceTime, distinctUntilChanged, map, switchMap, tap} from 'rxjs/operators';
-import {fromEvent, of} from 'rxjs';
+import {catchError, debounceTime, distinctUntilChanged, filter, map, switchMap} from 'rxjs/operators';
+import {fromEvent, Observable, of} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
-// helper functions
-const getContinents = keys =>
-  [
-    'africa',
-    'antarctica',
-    'asia',
-    'australia',
-    'europe',
-    'north america',
-    'south america'
-  ].filter(e => e.indexOf(keys.toLowerCase()) > -1);
+interface ICountry {
+  name: string;
+  capital: string;
+  flag: string;
+}
 
-const fakeContinentsRequest = keys =>
-  of(getContinents(keys)).pipe(
-    tap(_ => console.log(`API CALL at ${new Date()}`))
-  );
+const errorCountry: ICountry = {
+  name: 'Error',
+  capital: 'Not found',
+  flag: ''
+};
 
 @Component({
   selector: 'app-typeahead-stream',
@@ -27,19 +23,39 @@ const fakeContinentsRequest = keys =>
 export class TypeaheadStreamComponent implements OnInit {
 
   @ViewChild('typeahead', {static: true}) typeahead;
-  constructor() {
+  countries$: Observable<ICountry[]>;
+
+  constructor(private http: HttpClient) {
   }
 
   ngOnInit(): void {
-    fromEvent(this.typeahead.nativeElement, 'keyup')
+    // 1. In the template we loop over countries$ by using the async pipe
+    this.countries$ = fromEvent(this.typeahead.nativeElement, 'keyup')
       .pipe(
-        debounceTime(200),
+        // 2. There should be at least 2 characters in the textbox
+        filter((e: any) => e.target.value.length >= 2),
+        // 3. Wait for 400ms to continue, to prevent unnecessary http-calls
+        debounceTime(400),
+        // 4. Map out the value of the textbox, we want only the characters
         map((e: any) => e.target.value),
+        //  5. Only emit when the current value is different than the last
         distinctUntilChanged(),
-        switchMap(fakeContinentsRequest),
-        tap(c => (document.getElementById('output').innerText = c.join('\n')))
-      )
-      .subscribe();
+        // 6. Pass keyword and switch execution to getCountries http-call
+        switchMap(keyword => this.getCountries(keyword))
+      );
   }
 
+  getCountries(keyword): Observable<ICountry[]> {
+    // 7. Create the actual http-call.
+    // We could/should store the http-address in a
+    // separate variable, this method inside a service, and so on.
+    return this.http.get<ICountry[]>(`https://restcountries.eu/rest/v2/name/${keyword}?fields=name;capital;flag`)
+      .pipe(
+        // 8. catch http-errors and return a 'not found' country
+        catchError(err => {
+          console.log(err);
+          return of([errorCountry]);
+        })
+      );
+  }
 }
